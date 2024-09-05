c1fafefce0f3c7211cfb620188d74ce7599d726b
MayteHdez <maytehernandez22699@gmail.com>
2024-09-04 19:06:36 -0600

    ejercicio 3

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 <?php
+    echo "<p>Ejercicio 1</p>";
     $_myvar = "_myvar";
     $_7var = "_7var";
     $myvar = "myvar";
@@ -13,7 +14,6 @@
     $_element1 = "_element1";
     //$house*5 = "house*5"; Marca un error de sintaxis
     
-    echo "<p>Ejercicio 1</p>";
     echo "<p>$_myvar</p>";
     echo "<p>$_7var</p>";
     echo "<p>$myvar</p>";
@@ -22,6 +22,7 @@
     //echo "<p>$house*5 </p>";
 
     unset($_myvar, $_7var, $myvar, $var7, $_element1);
+
     echo "<br><p>Ejercicio 2</p>";
     $a = "ManejadorSQL";
     $b = 'MySQL';
@@ -39,6 +40,37 @@
     echo "<p>$a</p>";
     echo "<p>$b</p>";
     echo "<p>$c</p>";
+
+    unset($a, $b, $c);
+
+    echo "<br><p>Ejercicio 3</p>";
+    $a = "PHP5";
+    echo "<p>$a (tipo: " . gettype($a) . ")</p>";
+
+    $z[] = &$a;
+    echo "<p>{$z[0]} (tipo: " . gettype($z) . ")</p>";
+
+    $b = "5a version de PHP";
+    echo "<p>$b (tipo: " . gettype($b) . ")</p>";
+
+    $c = $b*10;
+    echo "<p>$c (tipo: " . gettype($c) . ")</p>";
+
+    $a .= $b;
+    echo "<p>$a (tipo: " . gettype($a) . ")</p>";
+
+    $b *= $c;
+    echo "<p> $b (tipo: " . gettype($b) . ")</p>";
+
+    $z[0] = "MySQL";
+    echo "<p>{$z[0]} (tipo: " . gettype($z) . ")</p>";
+
+    echo "<p>Componentes del array \$z:</p>";
+    echo "<pre>";
+    print_r($z);
+    echo "</pre>";
+
+    unset($a, $b, $c, $z);
 
     ?> 
 </body>
